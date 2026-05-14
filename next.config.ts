12a4836fba39f31fab687aefa7ff7be0a2f3a210
Mashudur Rahman (Mahir) <mdmashudurr81@gmail.com>
2026-05-14 12:28:59 +0600

    Next Config Fixed and fix the full proejct;

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        pathname: "/**",
+      },
+    ],
+  },
   async headers() {
     return [
       {
         source: "/(.*)",
-        images: {
-          remotePatterns: [
-            {
-              protocol: "https",
-              hostname: "img.youtube.com",
-              pathname: "/**",
-            },
-          ],
-        },
+
         headers: [
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
